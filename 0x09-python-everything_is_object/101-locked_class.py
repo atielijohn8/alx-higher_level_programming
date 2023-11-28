a25b6e8b7f019168ed4e6da943c008cc6ce73593
atielijohn8 <atielijohn4@gmail.com>
2023-11-28 21:27:35 +0300

    opp.ext

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,7 +5,7 @@
 class LockedClass:
     """
     Prevent the user from calling new LockedClass attributes
-    for anything but attributes called (first_name).
+    for anything but attribtes called (first_name).
     """
 
     __slots__ = ["first_name"]
